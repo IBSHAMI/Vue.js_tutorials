cc718fb43777e744a27d8679fbae5069b2a4471f
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-08 18:37:32 +0800

    section 2.1 init created explored methods and some decratives functions

--- section-2-1-STARTER/app.js
+++ section-2-1-STARTER/app.js
@@ -1,14 +1,50 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data() { // the data is returned as an object and can be shown in the template as javascript expressions
         return { 
             firstName: 'jon', 
             lastName: 'snow',
             email: 'jonsnow@gmail.com',
+            url: 'https://google.com',
+        }
+    }, 
+    methods: { // methods are functions that can be called in the template
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}` // this refers to the data object
         }
     }
-}).mount('#app') // create a new Vue instance 
-                           // the vue is taken from the cdn link in the index.html file
-                           // the created app is then mounted to the div with id app in the index.html file
+}).mount('#app')          
+
+// create a new Vue instance 
+// the vue is taken from the cdn link in the index.html file
+// the created app is then mounted to the div with id app in the index.html file
+
+
+
+// change data inside the vue instance model 
+// setTimeout(() => {
+//     vm.firstName = 'Arya'
+// }, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Vue.createApp({
