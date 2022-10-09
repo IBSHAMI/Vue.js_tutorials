f02b5452e52101f93bab748d6fb0710ddc9b7f7e
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-09 14:46:13 +0800

    section 2.18 explored class attru dynamic binding

--- section-2-18-STARTER/app.js
+++ section-2-18-STARTER/app.js
@@ -1,2 +1,13 @@
 let vm = Vue.createApp({
-}).mount('#app')
+  data() {
+    return {
+      isPurple: false,
+      selectedColor: '',
+    };
+  },
+  computed: {
+    boxClasses() {
+      return { purple: this.isPurple };
+    },
+  },
+}).mount("#app");
