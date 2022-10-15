66eea139f8efca9802d82ba7271868f3d2c9c924
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-15 18:04:35 +0800

    section 3.1 project finished.

--- section-3-1-STARTER/app.js
+++ section-3-1-STARTER/app.js
@@ -7,14 +7,27 @@ let vm = Vue.createApp({
             rotateZ: 0,
         }
     },
+    computed: {
+        boxStyles() {
+            return {
+                transform: `perspective(${this.prespective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`
+            }
+        },
+    },
     methods: {
         reset () { 
             this.prespective = 100;
             this.rotateX = 0;
             this.rotateY = 0;
             this.rotateZ = 0;
-        }
+        }, 
+        async copy() {
+            let text = `perspective(${this.prespective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`;
+            await navigator.clipboard.writeText(text);
+
+            // show a message to user 
+            alert('Copied to clipboard');
+        },
     },
-    computed: {},
     watch: {},
 }).mount("#app");
