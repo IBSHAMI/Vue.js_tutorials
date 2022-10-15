26f999d43c40e26d2e5c730f041ea5986111d5ff
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-15 21:20:15 +0800

    section 4.1 explored all different types of lifecycle hooks.

--- section-4-1-STARTER/app.js
+++ section-4-1-STARTER/app.js
@@ -3,5 +3,44 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+    console.log("beforeCreate called");
+    console.log(this.message);
+  },
+  created() {
+    console.log("created called");
+    console.log(this.message);
+  },
+  beforeMount() {
+    console.log("beforeMount called");
+    console.log(this.$el);
+  },
+  mounted() {
+    console.log("mounted called");
+    console.log(this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate called");
+  },
+  updated() {
+    console.log("updated called");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount called");
+  },
+  unmounted() {
+    console.log("unmounted called");
   }
-}).mount('#app')
+})
+
+vm.mount("#app");
+
+
+
+
+// we can delay the mounting of the app
+// until we have the data we need ot other conditions are met
+// setTimeout(() => {
+//   vm.mount("#app")
+// }, 3000)
