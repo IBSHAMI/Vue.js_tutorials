99c5996cb1e9800f1457a41051f882de9494c2ab
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-16 16:32:21 +0800

    section 6: created a new project using vue-create and created the first components.

---
+++ vue-projects-vite/components/src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <p>>{{ msg }}e</p>
+</template>
+
+
+<script>
+export default {
+  name: 'App', 
+  data() { 
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
