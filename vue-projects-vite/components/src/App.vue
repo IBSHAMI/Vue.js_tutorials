fd65b108f3ccdb78cf5c9b3ba447652cd70485a4
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-16 19:03:55 +0800

    section 6: explored passing data from parent components to child components using :bind and props.

--- vue-projects-vite/components/src/App.vue
+++ vue-projects-vite/components/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <p>hello</p>
-    <Greetings />
+    <h3>hello</h3>
+    <greetings />
+    <User :age="age"/>
   </div>
 </template>
 
 <script>
 import Greetings from "@/components/Greetings.vue";
+import User from "@/components/User.vue";
+
 export default {
   name: "App",
   // we register vue component locally
@@ -14,6 +17,12 @@ export default {
   // we list the components we want to use in the components property
   components: {
     Greetings,
+    User
+  },
+  data() {
+    return {
+      age: 30
+    }
   },
 };
 </script>
---
+++ vue-projects-vite/components/src/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p> the user is {{ age }} years old</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "user",
+    // we can use props to pass data from parent to child
+    // we use the same 
+    props: ["age"],
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
