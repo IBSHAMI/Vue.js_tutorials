5dbd7799e42624a90f9479c72faca0df83564bf9
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-17 18:50:53 +0800

    section 7 animations: created animation with css transition and css animating methods, and combine both

--- vue-projects-vite/animations/src/App.vue
+++ vue-projects-vite/animations/src/App.vue
@@ -1,14 +1,79 @@
 <template>
+  <div>
+    <button type="button" @click.prevent="show = !show">Toggle</button>
+    <!-- <transition name="fade" mode="out-in">
+      <h1 v-if="show" key="main">Animations</h1>
+      <h1 v-else key="second">Seond Animations</h1>
+    </transition> -->
 
+    <transition name="zoom" appear>
+      <h2 v-if="!show">Hello</h2>
+    </transition>
+  </div>
 </template>
 
 <script>
-export default { 
-  name: 'App',
-}
-
+export default {
+  name: "App",
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
 </script>
 
-<style scoped>
+<style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
 
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s linear;
+}
+.fade-leave-to {
+  transition: all 1s linear;
+  opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
+}
 </style>
